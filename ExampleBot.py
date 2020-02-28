74278071e4e1203847e903e8708935901b6a868f
ddthj <ddthj@users.noreply.github.com>
2020-02-27 21:20:36 -0500

    Added example of using raw utilities to example

--- ExampleBot.py
+++ ExampleBot.py
@@ -7,9 +7,17 @@ from routines import *
 
 class ExampleBot(GoslingAgent):
     def run(agent):
+        #An example of using raw utilities:
+        relative_target = agent.ball.location - agent.me.location
+        local_target = agent.me.local(relative_target)
+        defaultPD(agent, local_target)
+        defaultThrottle(agent, 2300)
+
+        #An example of pushing routines to the stack:
         if len(agent.stack) < 1:
             if agent.kickoff_flag:
                 agent.push(kickoff)
             else:
                 agent.push(atba())
+
         
